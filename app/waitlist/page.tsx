c8079adc4d62aeb0213880a4ae4dca9a383c637d
tali-creator <talinanzing111@gmail.com>
2025-05-30 18:20:22 +0100

    feat: Build 'Privacy & Security' and 'Using the Platform' FAQ UI in Therapy Hub

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Waitlist from '@/components/waitlist'
+import Faq from '@/components/user/therapy-hub/faq'
 
 const page = () => {
   return (
     <div className=''>
-      <Waitlist />
+      {/* <Waitlist /> */}
+      <Faq />
     </div>
   )
 }
