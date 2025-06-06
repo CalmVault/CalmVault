db798ccb215696f7843485ee342b9ab5a47d448f
igeorge17 <www.igeorge.www@gmail.com>
2025-06-06 02:50:14 +0100

    implemented therapist reputation score ui

---
+++ app/(therapist)/therapist-reputation-score/page.tsx
@@ -0,0 +1,14 @@
+import TherapistReputationScore from '@/components/user/therapist-reputation-score/therapist-reputation-score';
+
+
+const ReputationScore = () => {
+    return (
+        <>
+            <div className="w-full h-screen overflow-auto" >
+               <TherapistReputationScore />
+            </div>
+        </>
+    );
+}
+
+export default ReputationScore;
