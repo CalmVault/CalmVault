6244e761ada8ce82b59a130599a553e34bb10692
chukwu obed <codingwithsmart@gmail.com>
2025-06-18 20:35:45 +0100

    fix(Therapist-earnings):  move the Therapist-earning route from user to Therapist

--- app/(therapist)/therapist-earnings/page.tsx
+++ app/(therapist)/therapist-earnings/page.tsx
@@ -1,15 +1,7 @@
-
-
 import EarningsDashboard from "@/components/user/therapy-hub/therapist-earnings";
 
 const Page = () => {
-  return (
-    <>
-      <div>
-        <EarningsDashboard />
-      </div>
-    </>
-  );
+  return <EarningsDashboard />;
 };
 
 export default Page;
