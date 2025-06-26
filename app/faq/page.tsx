e6b453e8aeea74d35e67d3a9baaa93132b43f3af
legend4tech <ajuludennis14@gmail.com>
2025-06-26 06:48:14 +0100

    feat: fixed build errors

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import Faq from '@/components/user/therapy-hub/faq'
-import React from 'react'
-import { useRouter } from 'next/navigation';
+import Faq from "@/components/user/therapy-hub/faq";
+import React from "react";
+// import { useRouter } from "next/navigation";
 
 export default function Page() {
-    const router = useRouter();
-  
-  return (
-    <Faq />
-  );
+  // const router = useRouter();
+
+  return <Faq />;
 }
